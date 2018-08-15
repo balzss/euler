4a83d5559e5df60b788a4405ccf9ad48ae32e94d
Balazs Saros <bsaros@chemaxon.com>
2018-08-15 13:25:04 +0200

    p357 improvements

--- p357/p357.js
+++ p357/p357.js
@@ -4,44 +4,29 @@
 // It can be seen that for every divisor d of 30, d+30/d is prime.
 // Find the sum of all positive integers n not exceeding 100 000 000
 // such that for every divisor d of n, d+n/d is prime.
-let primeList = [];
 
 function isPrime(n) {
-    if(n === 2) return true;
-    if(n % 2 === 0) return false;
-    if(n === 1) return false;
-
     for (let i = 3; i < Math.sqrt(n); i += 2) {
-        if(n % i === 0) {
-            return false;
-        }
+        if(n % i === 0) return false;
     }
-
     return true;
 }
 
 function isInteresting(n) {
-    for(let i = 0; i < Math.sqrt(n); i++) {
-        if(n % i === 0) {
-            if(!isPrime(i + n/i)) return false;
-        }
+    for(let i = 1; i < Math.sqrt(n); i++) {
+        if(n % i === 0 && !isPrime(i + n/i)) return false;
     }
     return true;
 }
 
 let sum = 0;
+let last = 0;
 
-for(let i = 2; i < 100000000; i += 4) {
+// 1 and 4 are left out this way so the real solution is this + 5
+for(let i = 2; i < 10000000; i += 4) {
     if (isInteresting(i)) {
         sum += i;
     }
 }
 
-// sum = 0;
-// for(let i = 0; i < 4000000; i += 2) {
-//     if (isInteresting(i)) {
-//         sum += i;
-//     }
-// }
-
-console.log(sum);
+console.log(sum + 5);
